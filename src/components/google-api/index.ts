8c08ad7ca8a54a473daa9199edec1cdc21da3110
tigorthemusichead <arsenij.ermolaev2003@gmail.com>
2023-06-14 03:10:24 +0400

    creds

--- src/components/google-api/index.ts
+++ src/components/google-api/index.ts
@@ -2,13 +2,13 @@ const fs = require('fs')
 const {google} = require('googleapis')
 const path = require('path');
 
-// const KEYFILEPATH = path.resolve(__dirname) + '/credential.json'
+const KEYFILEPATH = path.resolve(__dirname) + '/credential.json'
 
 const SCOPES = ['https://www.googleapis.com/auth/drive']
 
 export const auth = new google.auth.GoogleAuth({
-    // keyFile: KEYFILEPATH,
-    credentials: {
+    keyFile: KEYFILEPATH,
+    /*credentials: {
         "type": "service_account",
         "project_id": process.env.CRED_PROJECT_ID,
         "private_key_id": process.env.CRED_PRIVATE_KEY_ID,
@@ -20,7 +20,7 @@ export const auth = new google.auth.GoogleAuth({
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
         "client_x509_cert_url": process.env.CRED_AUTH_X509_CERT_URL,
         "universe_domain": "googleapis.com"
-    },
+    },*/
     scopes: SCOPES
 })
 
